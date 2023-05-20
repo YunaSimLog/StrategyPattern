2c0050e5730151d7cc3847b56f70435b809d6b1e
yunasim <ynsim@solidit.co.kr>
2023-05-20 14:45:40 +0900

    #✨feature: 초기 업로드.

---
+++ FlyMode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public class CanFlyMode : IFlyBehavior
+    {
+        public void fly()
+        {
+            Console.WriteLine("날다");
+        }
+    }
+
+    public class CanNotFlyMode:IFlyBehavior
+    {
+        public void fly()
+        {
+            Console.WriteLine("못 난다");
+        }
+    }
+}
---
+++ QuackMode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public class Cackle : IQuackBehavior
+    {
+        public void Quak()
+        {
+            Console.WriteLine("꽥꽥");
+        }
+    }
+
+    public class BeepBeep : IQuackBehavior
+    {
+        public void Quak()
+        {
+            Console.WriteLine("삑삑");
+        }
+    }
+}
